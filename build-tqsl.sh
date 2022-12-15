d66581c862517f63a87c464aca46e9b953161813
Loren M. Lang <lorenl@north-winds.org>
2022-12-14 16:44:06 -0800

    Initial version of TrustedQSL builder scripts

---
+++ build-tqsl.sh
@@ -0,0 +1,140 @@
+#!/bin/sh
+
+set -e
+
+. /etc/os-release
+
+if [ "$VERSION_ID" = "14.04" ]; then
+	VERSION_CODENAME="trusty"
+fi
+
+#branch="backport-focal"
+#branch="backport-impish"
+#branch="backport-jammy"
+#branch="backport-kinetic"
+branch="backport-${VERSION_CODENAME}"
+
+export DISPLAY=":0"
+export SSH_AUTH_SOCK="/tmp/ssh-agent.sock"
+export GPG_AGENT_INFO="$(gpgconf --list-dir | grep '^agent-socket:' | cut -d: -f2):0:1"
+if tty >/dev/null 2>&1; then
+	export GPG_TTY=$(tty)
+fi
+
+echo 'Acquire::http::Proxy "http://10.146.39.1:3142";' | sudo tee /etc/apt/apt.conf.d/00aptproxy
+
+if [ "${VERSION_CODENAME}" = "groovy" -o \
+     "${VERSION_CODENAME}" = "hirsute" -o \
+     "${VERSION_CODENAME}" = "impish" ]; then
+	sudo sed -i 's:archive.ubuntu.com:old-releases.ubuntu.com:' /etc/apt/sources.list
+	sudo sed -i 's:security.ubuntu.com:old-releases.ubuntu.com:' /etc/apt/sources.list
+fi
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -qy
+#sudo DEBIAN_FRONTEND=noninteractive apt install -y wget dpkg-dev gcc cmake libssl-dev liblmdb-dev libdb5.3-dev libexpat1-dev zlib1g-dev libcurl4-gnutls-dev libwxgtk3.0-gtk3-dev libfuse2 trustedqsl #fuse3
+#rm -fr ~/raw
+#mkdir ~/raw
+#cd ~/raw
+#wget http://www.arrl.org/tqsl/tqsl-2.6.5.tar.gz
+#rm -fr tqsl-2.6.5
+#tar xvf tqsl-2.6.5.tar.gz
+#cd tqsl-2.6.5
+#cmake -B build -S .
+#cmake --build build
+#sudo cmake --install build
+#sudo ldconfig
+#tqsl --version 2>&1 | grep --color 'TQSL Version 2\..*'
+#sudo mkdir -p /usr/local/bin/
+## FIXME: Workaround needed on older Ubuntu releases
+## error: RPC failed; curl 56 GnuTLS recv error (-54): Error in the pull function.
+#git config --global http.postBuffer 1048576000
+#./linux-make-appimage.sh
+#./TQSL-x86_64.AppImage --version 2>&1 | grep --color 'TQSL Version 2\..*'
+##./linux-make-appimage.sh || true
+##./TQSL-x86_64.AppImage --version 2>&1 | grep --color 'TQSL Version 2\..*' || true
+cd
+
+
+paramiko=python3-paramiko
+if [ "$branch" = "backport-trusty" ]; then
+	paramiko="python-paramiko python-bzrlib"
+fi
+sudo DEBIAN_FRONTEND=noninteractive apt install -qy devscripts equivs git-buildpackage gnupg2 $paramiko
+if [ "$branch" = "backport-xenial" ]; then
+	sudo DEBIAN_FRONTEND=noninteractive apt remove -qy gnupg
+fi
+export DEBEMAIL="lorenl@north-winds.org"
+export DEBFULLNAME="Loren M. Lang"
+
+cat >.devscripts <<EOF
+DEBUILD_DPKG_BUILDPACKAGE_OPTS="-i -I -us -uc"
+#DEBUILD_LINTIAN_OPTS="-i -I --show-overrides"
+DEBSIGN_KEYID="7896E0999FC79F6CE0EDE103222DF356A57A98FA"
+DSCVERIFY_KEYRINGS="$HOME/.gnupg/trustedkeys.gpg"
+EOF
+cat >.dput.cf <<EOF
+[trustedqsl]
+fqdn = ppa.launchpad.net
+incoming = ~penguin359/ubuntu/trustedqsl/
+method = sftp
+login = penguin359
+EOF
+
+mkdir -p ~/.ssh/
+cat >> ~/.ssh/known_hosts <<EOF
+|1|L65WTC0AxkfE4nrvu2RV0ZmhURg=|wRCkGhAHG+NTMvWJZhrOk1Y/o1s= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA0aKz5UTUndYgIGG7dQBV+HaeuEZJ2xPHo2DS2iSKvUL4xNMSAY4UguNW+pX56nAQmZKIZZ8MaEvSj6zMEDiq6HFfn5JcTlM80UwlnyKe8B8p7Nk06PPQLrnmQt5fh0HmEcZx+JU9TZsfCHPnX7MNz4ELfZE6cFsclClrKim3BHUIGq//t93DllB+h4O9LHjEUsQ1Sr63irDLSutkLJD6RXchjROXkNirlcNVHH/jwLWR5RcYilNX7S5bIkK8NlWPjsn/8Ua5O7I9/YoE97PpO6i73DTGLh5H9JN/SITwCKBkgSDWUt61uPK3Y11Gty7o2lWsBjhBUm2Y38CBsoGmBw==
+|1|xPW80Prm6bDJ95oIpf2oWCo2ZbM=|f/tCGjfc5/6MrNIhruPMyX026nc= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA0aKz5UTUndYgIGG7dQBV+HaeuEZJ2xPHo2DS2iSKvUL4xNMSAY4UguNW+pX56nAQmZKIZZ8MaEvSj6zMEDiq6HFfn5JcTlM80UwlnyKe8B8p7Nk06PPQLrnmQt5fh0HmEcZx+JU9TZsfCHPnX7MNz4ELfZE6cFsclClrKim3BHUIGq//t93DllB+h4O9LHjEUsQ1Sr63irDLSutkLJD6RXchjROXkNirlcNVHH/jwLWR5RcYilNX7S5bIkK8NlWPjsn/8Ua5O7I9/YoE97PpO6i73DTGLh5H9JN/SITwCKBkgSDWUt61uPK3Y11Gty7o2lWsBjhBUm2Y38CBsoGmBw==
+EOF
+
+# dch -v 2.6.5-3~penguin359~kinetic1
+rm -f ~/.gnupg/trustedkeys.gpg
+gpg2 --keyserver keyserver.ubuntu.com --recv-key "7896E0999FC79F6CE0EDE103222DF356A57A98FA"
+#echo -e "5\ny\nsave\n" | gpg2 --command-fd 0 --edit-key "7896E0999FC79F6CE0EDE103222DF356A57A98FA" trust
+gpg2 --import-ownertrust <<EOF
+7896E0999FC79F6CE0EDE103222DF356A57A98FA:6:
+EOF
+gpg2 --check-trustdb
+gpg2 --output ~/.gnupg/trustedkeys.gpg --export "7896E0999FC79F6CE0EDE103222DF356A57A98FA"
+
+rm -fr ~/deb
+mkdir ~/deb
+cd ~/deb
+rm -fr trustedqsl
+git clone -b "$branch" https://github.com/penguin359/trustedqsl
+mk-build-deps trustedqsl/debian/control
+if [ "$branch" = "backport-trusty" ]; then
+	sudo DEBIAN_FRONTEND=noninteractive dpkg -i ./trustedqsl-build-deps_*.deb || true
+	sudo apt install -y -f
+else
+	sudo DEBIAN_FRONTEND=noninteractive apt install -qy ./trustedqsl-build-deps_*.deb
+fi
+wget http://archive.ubuntu.com/ubuntu/pool/universe/t/trustedqsl/trustedqsl_2.6.5.orig.tar.gz
+cd trustedqsl
+lintian_opts="--fail-on error,warning"
+if [ "$branch" = "backport-trusty" -o \
+     "$branch" = "backport-xenial" -o \
+     "$branch" = "backport-bionic" -o \
+     "$branch" = "backport-focal" ]; then
+	lintian_opts=""
+fi
+#dpkg-buildpackage -kfakeroot
+#gbp buildpackage --git-debian-branch="$branch" --git-tarball-dir=.. --lintian-opts $lintian_opts
+echo "===> Building binary package..."
+gbp buildpackage --git-debian-branch="$branch" --git-tarball-dir=.. --git-builder="debuild --no-lintian -i -I"
+#lintian -i -I --fail-on error,warning,info,pedantic ../trustedqsl_*_amd64.changes
+echo "===> Running lintian on binary package..."
+lintian -I --pedantic $lintian_opts ../trustedqsl_*_amd64.changes
+echo "===> Building source package..."
+debuild --no-lintian -S
+echo "===> Running lintian on source package..."
+lintian -I --pedantic $lintian_opts ../trustedqsl_*_source.changes
+#debsign
+debsign -S
+cd ..
+sudo dpkg -i trustedqsl_*_amd64.deb
+# Exits with status 255 on good invocation
+tqsl --version 2>&1 | grep --color 'TQSL Version 2\..*'
+dscverify trustedqsl_*_source.changes
+dput -ol trustedqsl trustedqsl_*_source.changes
+dput --debug -l trustedqsl trustedqsl_*_source.changes
+echo "Success!"
---
+++ tqsl.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+set -e
+
+container="tqsl3"
+release="ubuntu:14.04"
+#release="ubuntu:16.04"
+#release="ubuntu:18.04"
+#release="ubuntu:20.04"
+#release="ubuntu:20.10"
+#release="ubuntu:21.04"
+#release="ubuntu:21.10"
+#release="ubuntu:22.04"
+#release="ubuntu:22.10"
+#release="ubuntu:23.04"
+#release="ubuntu:devel"
+
+user="ubuntu"
+
+lxc stop --force "$container" 2>/dev/null || true
+lxc delete "$container" 2>/dev/null || true
+echo "===> Creating new container..."
+lxc launch "$release" "$container"
+#lxc start "$container"
+echo "===> Waiting for start-up..."
+if [ "$release" = "ubuntu:16.04" ]; then
+	lxc exec "$container" -- dhclient eth0
+	lxc exec "$container" -- sh -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
+	lxc exec "$container" -- sed -i 's:\( localhost\):\1 '"$container"':' /etc/hosts
+fi
+for i in $(seq 10); do
+	if lxc exec "$container" -- ping -c 1 www.google.com; then
+		break
+	fi
+	sleep 1
+done
+if [ "$release" = "ubuntu:16.04" ]; then
+	lxc exec "$container" -- useradd "$user" -m -G sudo -s /bin/bash
+	lxc exec "$container" -- sh -c 'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/99-ubuntu-user'
+	lxc exec "$container" -- apt install -y gnupg2
+	lxc exec "$container" -- rm -f /etc/apt/sources.list.d/ubuntu-esm-infra.list
+elif [ "$release" = "ubuntu:14.04" ]; then
+	lxc exec "$container" -- apt install -y gnupg2
+	lxc exec "$container" -- rm -f /etc/apt/sources.list.d/ubuntu-esm-infra-trusty.list
+else
+	lxc exec "$container" -- systemctl isolate default
+fi
+home="$(lxc exec "$container" -- sudo -u "$user" -i echo '$HOME')"
+uid="$(lxc exec "$container" -- sudo -u "$user" -i id -u)"
+gid="$(lxc exec "$container" -- sudo -u "$user" -i id -g)"
+host_socket="$(gpgconf --list-dir agent-extra-socket)"
+#container_socket="$(lxc exec "$container" -- sudo -u "$user" -i gpgconf --list-dir agent-socket)"
+container_socket="$(lxc exec "$container" -- sudo -u "$user" -i gpgconf --list-dir | grep '^agent-socket:' | cut -d: -f2)"
+x11_socket="/tmp/.X11-unix/X$(echo "$DISPLAY" | cut -d: -f2 | cut -d. -f1)"
+my_ip="$(ip -o route get 240.0.0.0 | sed -n 's:.* src \([^ ]\+\) .*:\1:p')"
+lxc exec "$container" -- sudo -u "$user" -i mkdir -m 700 -p "$(dirname "$container_socket")"
+lxc config device remove "$container" gpg-agent 2>/dev/null || true
+lxc config device remove "$container" ssh-agent 2>/dev/null || true
+lxc config device remove "$container" x11 2>/dev/null || true
+lxc config device add "$container" ssh-agent proxy connect=unix:"$SSH_AUTH_SOCK" listen=unix:"/tmp/ssh-agent.sock" bind=container uid="$uid" gid="$gid" mode=0600
+lxc config device add "$container" x11 disk source="$x11_socket" path=/tmp/.X11-unix/X0
+xauth extract - "$DISPLAY" | lxc exec "$container" -- sudo -u "$user" -i xauth merge -
+if [ "$release" = "ubuntu:14.04" -o \
+     "$release" = "ubuntu:16.04" ]; then
+	gpg2 --export-secret-key "7896E0999FC79F6CE0EDE103222DF356A57A98FA" | lxc exec "$container" -- sudo -u "$user" -i gpg2 --batch --import
+else
+	lxc config device add "$container" gpg-agent proxy connect=unix:"$host_socket" listen=unix:"$container_socket" bind=container uid="$uid" gid="$gid" mode=0600
+fi
+lxc file push build-tqsl.sh "$container""$home"/
+echo "===> Starting build script..."
+lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl.sh"
+#lxc stop "$container"
+#lxc delete "$container"
+echo "===> Done."
