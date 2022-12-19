05ac3b3d47f42d6513e3e79b7f3830b5e24173bd
Loren M. Lang <lorenl@north-winds.org>
2022-12-19 03:15:09 -0800

    Enable color and debugging symbols in watcher script

--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -5,10 +5,10 @@ base="$(dirname "$(readlink -f "$0")")"
 cd "${base}"
 
 test() {
-	cmake -B ~/raw/tqsl-build -S ~/raw/tqsl && cmake --build ~/raw/tqsl-build && ctest --test-dir ~/raw/tqsl-build/tests/ -V
+	cmake -DCMAKE_BUILD_TYPE=Debug -B ~/raw/tqsl-build -S ~/raw/tqsl && cmake --build ~/raw/tqsl-build && GTEST_COLOR=1 ctest --test-dir ~/raw/tqsl-build/tests/ -V
 }
 
 test
-while inotifywait --exclude '.*(~|\.sw[a-p])$' --event CLOSE_WRITE -r ~/raw/tqsl/; do
+while inotifywait --exclude '.*(~|\.sw[p-z])$' --event CLOSE_WRITE -r ~/raw/tqsl/; do
 	test
 done
