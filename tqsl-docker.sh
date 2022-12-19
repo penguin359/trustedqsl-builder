91d8ddf22d651ce11849cf3708eea00a20eb5726
Loren M. Lang <lorenl@north-winds.org>
2022-12-18 21:46:02 -0800

    Enable full argument parsing to configure behavior of builders

--- tqsl-docker.sh
+++ tqsl-docker.sh
@@ -1,36 +1,83 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+container="tqsl"
+
+args=$(getopt --name "$0" --options 'hn:' --longoptions 'help,name:' --shell sh -- "$@")
+if [ $? -ne 0 ]; then
+	echo >&2
+	echo "Invalid options, use -h for help." >&2
+	exit 1
+fi
+eval set -- "$args"
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h|--help)
+			echo "Usage: $0 [-h] [-n name] tag..." >&2
+			exit 0
+			;;
+		-n|--name)
+			container="$2"
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Error in getopt configuration (missing or extra options?)" >&2
+			exit 1
+			;;
+	esac
+	shift
+done
 
 set -e
 
-container="tqsl-fun"
+declare -a tags=($@)
 
-docker build --tag tqsl .
+if [ "${#tags[@]}" -eq 0 ]; then
+	tags=("22.10")
+fi
 
-host_socket="$(gpgconf --list-dir agent-extra-socket)"
-container_socket="$(docker run --rm tqsl gpgconf --list-dir | grep '^agent-socket:' | cut -d: -f2)"
-x11_socket="/tmp/.X11-unix/X$(echo "$DISPLAY" | cut -d: -f2 | cut -d. -f1)"
-echo "===> Detected sockets..."
-echo "host_socket=$host_socket"
-echo "container_socket=$container_socket"
-echo "x11_socket=$x11_socket"
-echo
-echo "===> Starting build script..."
-docker container run -it --rm \
-	-v "${SSH_AUTH_SOCK}:/tmp/ssh-agent.sock" \
-	-v "${x11_socket}:/tmp/.X11-unix/X0" \
-	-v "${host_socket}:${container_socket}" \
-	--name "$container" tqsl
-	#-v "${HOME}/.gnupg:/home/ubuntu/.gnupg" \
-	#-v /tmp/.X11-unix:/tmp/.X11-unix \
-	#-e DISPLAY="$DISPLAY" \
-docker container run -it --rm \
-	-v "${x11_socket}:/tmp/.X11-unix/X0" \
-	--name "$container" tqsl ./build-tqsl-tarball.sh
-docker container run -it --rm \
-	-v "${x11_socket}:/tmp/.X11-unix/X0" \
-	--device /dev/fuse \
-	--cap-add SYS_ADMIN \
-	--security-opt apparmor:unconfined \
-	--name "$container" tqsl ./build-tqsl-appimage.sh
+build() {
+	local tag="$1"
+	local release="ubuntu:${tag}"
 
-echo "===> Done."
+	sed -i -e 's/^FROM .*/FROM '"$release"'/' Dockerfile
+
+	docker build --tag tqsl .
+
+	host_socket="$(gpgconf --list-dir agent-extra-socket)"
+	container_socket="$(docker run --rm tqsl gpgconf --list-dir | grep '^agent-socket:' | cut -d: -f2)"
+	x11_socket="/tmp/.X11-unix/X$(echo "$DISPLAY" | cut -d: -f2 | cut -d. -f1)"
+	echo "===> Detected sockets..."
+	echo "host_socket=$host_socket"
+	echo "container_socket=$container_socket"
+	echo "x11_socket=$x11_socket"
+	echo
+	echo "===> Starting build script..."
+	docker container run -it --rm \
+		-v "${SSH_AUTH_SOCK}:/tmp/ssh-agent.sock" \
+		-v "${x11_socket}:/tmp/.X11-unix/X0" \
+		-v "${host_socket}:${container_socket}" \
+		--name "$container" tqsl
+		#-v "${HOME}/.gnupg:/home/ubuntu/.gnupg" \
+		#-v /tmp/.X11-unix:/tmp/.X11-unix \
+		#-e DISPLAY="$DISPLAY" \
+	docker container run -it --rm \
+		-v "${x11_socket}:/tmp/.X11-unix/X0" \
+		--name "$container" tqsl ./build-tqsl-tarball.sh
+	docker container run -it --rm \
+		-v "${x11_socket}:/tmp/.X11-unix/X0" \
+		--device /dev/fuse \
+		--cap-add SYS_ADMIN \
+		--security-opt apparmor:unconfined \
+		--name "$container" tqsl ./build-tqsl-appimage.sh
+
+	echo "===> Done."
+}
+
+for i in "${tags[@]}"; do
+	build "$i"
+done
--- tqsl-lxc.sh
+++ tqsl-lxc.sh
@@ -1,90 +1,125 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+container="tqsl"
+
+args=$(getopt --name "$0" --options 'hn:' --longoptions 'help,name:' --shell sh -- "$@")
+if [ $? -ne 0 ]; then
+	echo >&2
+	echo "Invalid options, use -h for help." >&2
+	exit 1
+fi
+eval set -- "$args"
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-h|--help)
+			echo "Usage: $0 [-h] [-n name] tag..." >&2
+			exit 0
+			;;
+		-n|--name)
+			container="$2"
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "Error in getopt configuration (missing or extra options?)" >&2
+			exit 1
+			;;
+	esac
+	shift
+done
 
 set -e
 
-tag=
-if [ $# -eq 1 ]; then
-	tag="$1"
+declare -a tags=($@)
+
+if [ "${#tags[@]}" -eq 0 ]; then
+	#tags=("14.04")
+	#tags=("16.04")
+	#tags=("18.04")
+	#tags=("20.04")
+	#tags=("20.10")
+	#tags=("21.04")
+	#tags=("21.10")
+	#tags=("22.04")
+	tags=("22.10")
+	#tags=("23.04")
+	#tags=("devel")
 fi
 
-container="tqsl"
-if [ -z "$tag" ]; then
-	tag="14.04"
-	#tag="16.04"
-	#tag="18.04"
-	#tag="20.04"
-	#tag="20.10"
-	#tag="21.04"
-	#tag="21.10"
-	#tag="22.04"
-	#tag="22.10"
-	#tag="23.04"
-	#tag="devel"
-fi
-release="ubuntu:${tag}"
+build() {
+	local tag="$1"
+	local release="ubuntu:${tag}"
+	local user="ubuntu"
 
-user="ubuntu"
-
-lxc stop --force "$container" 2>/dev/null || true
-lxc delete "$container" 2>/dev/null || true
-echo "===> Creating new container for ${release}..."
-lxc launch "$release" "$container"
-#lxc start "$container"
-echo "===> Waiting for start-up..."
-if [ "$release" = "ubuntu:16.04" ]; then
-	lxc exec "$container" -- dhclient eth0
-	lxc exec "$container" -- sh -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
-	lxc exec "$container" -- sed -i 's:\( localhost\):\1 '"$container"':' /etc/hosts
-fi
-for i in $(seq 10); do
-	if lxc exec "$container" -- ping -c 1 www.google.com; then
-		break
+	lxc stop --force "$container" 2>/dev/null || true
+	lxc delete "$container" 2>/dev/null || true
+	echo "===> Creating new container for ${release}..."
+	lxc launch "$release" "$container"
+	#lxc start "$container"
+	echo "===> Waiting for start-up..."
+	if [ "$release" = "ubuntu:16.04" ]; then
+		lxc exec "$container" -- dhclient eth0
+		lxc exec "$container" -- sh -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
+		lxc exec "$container" -- sed -i 's:\( localhost\):\1 '"$container"':' /etc/hosts
 	fi
-	sleep 1
+	for i in $(seq 10); do
+		if lxc exec "$container" -- ping -c 1 www.google.com; then
+			break
+		fi
+		sleep 1
+	done
+	if [ "$release" = "ubuntu:16.04" ]; then
+		lxc exec "$container" -- useradd "$user" -m -G sudo -s /bin/bash
+		lxc exec "$container" -- sh -c 'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/99-ubuntu-user'
+		lxc exec "$container" -- apt install -y gnupg2
+		lxc exec "$container" -- rm -f /etc/apt/sources.list.d/ubuntu-esm-infra.list
+	elif [ "$release" = "ubuntu:14.04" ]; then
+		lxc exec "$container" -- apt install -y gnupg2
+		lxc exec "$container" -- rm -f /etc/apt/sources.list.d/ubuntu-esm-infra-trusty.list
+	else
+		lxc exec "$container" -- systemctl isolate default
+	fi
+	local home="$(lxc exec "$container" -- sudo -u "$user" -i echo '$HOME')"
+	local uid="$(lxc exec "$container" -- sudo -u "$user" -i id -u)"
+	local gid="$(lxc exec "$container" -- sudo -u "$user" -i id -g)"
+	local host_socket="$(gpgconf --list-dir agent-extra-socket)"
+	#local container_socket="$(lxc exec "$container" -- sudo -u "$user" -i gpgconf --list-dir agent-socket)"
+	local container_socket="$(lxc exec "$container" -- sudo -u "$user" -i gpgconf --list-dir | grep '^agent-socket:' | cut -d: -f2)"
+	local x11_socket="/tmp/.X11-unix/X$(echo "$DISPLAY" | cut -d: -f2 | cut -d. -f1)"
+	local my_ip="$(ip -o route get 240.0.0.0 | sed -n 's:.* src \([^ ]\+\) .*:\1:p')"
+	lxc exec "$container" -- sudo -u "$user" -i mkdir -m 700 -p "$(dirname "$container_socket")" "$home"/.gnupg
+	lxc config device remove "$container" gpg-agent 2>/dev/null || true
+	lxc config device remove "$container" ssh-agent 2>/dev/null || true
+	lxc config device remove "$container" x11 2>/dev/null || true
+	lxc config device add "$container" ssh-agent proxy connect=unix:"$SSH_AUTH_SOCK" listen=unix:"/tmp/ssh-agent.sock" bind=container uid="$uid" gid="$gid" mode=0600
+	#lxc config device add "$container" x11 disk source="$x11_socket" path=/tmp/.X11-unix/X0
+	lxc config device add "$container" x11 proxy connect=unix:"$x11_socket" listen=unix:/tmp/.X11-unix/X0 bind=container uid="$uid" gid="$gid" mode=0600
+	xauth extract - "$DISPLAY" | lxc exec "$container" -- sudo -u "$user" -i xauth merge -
+	if [ "$release" = "ubuntu:14.04" -o \
+	     "$release" = "ubuntu:16.04" ]; then
+		gpg2 --export-secret-key "7896E0999FC79F6CE0EDE103222DF356A57A98FA" | lxc exec "$container" -- sudo -u "$user" -i gpg2 --batch --import
+	else
+		lxc config device add "$container" gpg-agent proxy connect=unix:"$host_socket" listen=unix:"$container_socket" bind=container uid="$uid" gid="$gid" mode=0600
+	fi
+	echo "===> Detected sockets..."
+	echo "host_socket=$host_socket"
+	echo "container_socket=$container_socket"
+	echo "x11_socket=$x11_socket"
+	echo
+	lxc file push -r scripts/* "$container""$home"/
+	echo "===> Starting build script..."
+	lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl-package.sh"
+	lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl-tarball.sh"
+	lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl-appimage.sh"
+	lxc stop "$container"
+	lxc delete "$container"
+	echo "===> Done."
+}
+
+for i in "${tags[@]}"; do
+	build "$i"
 done
-if [ "$release" = "ubuntu:16.04" ]; then
-	lxc exec "$container" -- useradd "$user" -m -G sudo -s /bin/bash
-	lxc exec "$container" -- sh -c 'echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/99-ubuntu-user'
-	lxc exec "$container" -- apt install -y gnupg2
-	lxc exec "$container" -- rm -f /etc/apt/sources.list.d/ubuntu-esm-infra.list
-elif [ "$release" = "ubuntu:14.04" ]; then
-	lxc exec "$container" -- apt install -y gnupg2
-	lxc exec "$container" -- rm -f /etc/apt/sources.list.d/ubuntu-esm-infra-trusty.list
-else
-	lxc exec "$container" -- systemctl isolate default
-fi
-home="$(lxc exec "$container" -- sudo -u "$user" -i echo '$HOME')"
-uid="$(lxc exec "$container" -- sudo -u "$user" -i id -u)"
-gid="$(lxc exec "$container" -- sudo -u "$user" -i id -g)"
-host_socket="$(gpgconf --list-dir agent-extra-socket)"
-#container_socket="$(lxc exec "$container" -- sudo -u "$user" -i gpgconf --list-dir agent-socket)"
-container_socket="$(lxc exec "$container" -- sudo -u "$user" -i gpgconf --list-dir | grep '^agent-socket:' | cut -d: -f2)"
-x11_socket="/tmp/.X11-unix/X$(echo "$DISPLAY" | cut -d: -f2 | cut -d. -f1)"
-my_ip="$(ip -o route get 240.0.0.0 | sed -n 's:.* src \([^ ]\+\) .*:\1:p')"
-lxc exec "$container" -- sudo -u "$user" -i mkdir -m 700 -p "$(dirname "$container_socket")" "$home"/.gnupg
-lxc config device remove "$container" gpg-agent 2>/dev/null || true
-lxc config device remove "$container" ssh-agent 2>/dev/null || true
-lxc config device remove "$container" x11 2>/dev/null || true
-lxc config device add "$container" ssh-agent proxy connect=unix:"$SSH_AUTH_SOCK" listen=unix:"/tmp/ssh-agent.sock" bind=container uid="$uid" gid="$gid" mode=0600
-#lxc config device add "$container" x11 disk source="$x11_socket" path=/tmp/.X11-unix/X0
-lxc config device add "$container" x11 proxy connect=unix:"$x11_socket" listen=unix:/tmp/.X11-unix/X0 bind=container uid="$uid" gid="$gid" mode=0600
-xauth extract - "$DISPLAY" | lxc exec "$container" -- sudo -u "$user" -i xauth merge -
-if [ "$release" = "ubuntu:14.04" -o \
-     "$release" = "ubuntu:16.04" ]; then
-	gpg2 --export-secret-key "7896E0999FC79F6CE0EDE103222DF356A57A98FA" | lxc exec "$container" -- sudo -u "$user" -i gpg2 --batch --import
-else
-	lxc config device add "$container" gpg-agent proxy connect=unix:"$host_socket" listen=unix:"$container_socket" bind=container uid="$uid" gid="$gid" mode=0600
-fi
-echo "===> Detected sockets..."
-echo "host_socket=$host_socket"
-echo "container_socket=$container_socket"
-echo "x11_socket=$x11_socket"
-echo
-lxc file push -r scripts/* "$container""$home"/
-echo "===> Starting build script..."
-lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl-package.sh"
-lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl-tarball.sh"
-lxc exec "$container" -- sudo -u "$user" -i "./build-tqsl-appimage.sh"
-lxc stop "$container"
-lxc delete "$container"
-echo "===> Done."
