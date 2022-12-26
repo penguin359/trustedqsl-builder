7aae16fb19390fc31987c61ec23d3c8937e70444
Loren M. Lang <lorenl@north-winds.org>
2022-12-26 15:54:15 -0800

    Check downloaded file hash and ensure mounts are always cleaned up

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,65 +2,133 @@
 
 $ErrorActionPreference = "Stop"
 
+#$scriptDir = $PSScriptRoot
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-#Write-Output $PSScriptRoot
-#Write-Output $scriptDir
-#return 0
+
 Set-Location -LiteralPath $scriptDir
 
-if(-not(Test-Path -Path downloads)) {
-	New-Item -Name downloads -Type Directory | Out-Null
+function Test-CommandExists {
+	param(
+		[Parameter(Mandatory)]
+		[string]$Command
+	)
+
+	try {
+		$oldErrorAction = $ErrorActionPreference
+		$ErrorActionPreference = "stop"
+		if(Get-Command $Command) {
+			return $true
+		}
+	} catch {
+		# Suppress the exception
+	} finally {
+		$ErrorActionPreference = $oldErrorAction
+	}
+
+	return $false
 }
 
-$url = "http://download.microsoft.com/download/8/B/5/8B5804AD-4990-40D0-A6AA-CE894CBBB3DC/VS2008ExpressENUX1397868.iso"
-$file = "downloads\VS2008ExpressENUX1397868.iso"
-$hash = "632318EF0DF5BAD58FCB99852BD251243610E7A4D84213C45B4F693605A13EAD"
+function Test-FileHash {
+	param(
+		[Parameter(Mandatory)]
+		[string]$File,
 
-Write-Output "Checking for existing download of VC++ 2008..."
-if(-not(Test-Path -Path $file) -or (Get-FileHash -Path $file -Algorithm SHA256).Hash -ne $hash) {
-	Remove-Item -Force -Path $file -ErrorAction SilentlyContinue
-	Write-Output "Downloading VC++ 2008..."
-	Invoke-WebRequest -Uri $url -OutFile $file
-}
-if((Get-FileHash -Path $file -Algorithm SHA256).Hash -ne $hash) {
-	Write-Error "Downloaded file is corrupted!"
-	return 1
+		[Parameter()]
+		[string]$Hash
+	)
+
+	Write-Debug "File: $File, Hash: $Hash"
+	$Hash -eq "" -or `
+	(Get-FileHash -Path $File -Algorithm SHA256).Hash -eq $Hash
 }
 
-Write-Output "Installing prerequisites..."
+function Download-File {
+	param(
+		[Parameter(Mandatory)]
+		[string]$Url,
+
+		[Parameter(Mandatory)]
+		[string]$File,
+
+		[Parameter()]
+		[string]$Hash,
+
+		[Parameter()]
+		[string]$Name
+	)
+
+	if($Name -eq "") {
+		$Name = $File
+	}
+
+	$downloadDir = Join-Path $scriptDir (Join-Path "downloads" "b")
+	Write-Warning "$downloadDir"
+	$downloadFile = Join-Path $downloadDir $File
+	if(-not(Test-Path -Path $downloadDir)) {
+		New-Item -Path $downloadDir -Type Directory | Out-Null
+	}
+
+	Write-Verbose "Checking for existing download of ${Name}..."
+	if(-not(Test-Path -Path $downloadFile) -or
+	   -not(Test-FileHash $downloadFile $Hash)) {
+		Remove-Item -Force -Path $downloadFile -ErrorAction SilentlyContinue
+		Write-Verbose "Downloading ${Name}..."
+		Invoke-WebRequest -Uri $Url -OutFile $downloadFile
+		if(-not(Test-Path -Path $downloadFile) -or
+		   -not(Test-FileHash $downloadFile $Hash)) {
+			Write-Error "Downloaded file $downloadFile is missing or corrupted!"
+			return
+		}
+	}
+
+	Write-Output $downloadFile
+}
+
+$VisualStudio2008 = @{
+	Name = "VC++ 2008";
+	Url = "http://download.microsoft.com/download/8/B/5/8B5804AD-4990-40D0-A6AA-CE894CBBB3DC/VS2008ExpressENUX1397868.iso";
+	File = "VS2008ExpressENUX1397868.iso";
+	Hash = "632318EF0DF5BAD58FCB99852BD251243610E7A4D84213C45B4F693605A13EAD";
+}
+
+$imagePath = Download-File @VisualStudio2008
+
+Write-Verbose "Installing prerequisites..."
 choco install dotnet3.5
 
-Write-Output "Mounting disk image..."
-$disk = Mount-DiskImage -ImagePath "${scriptDir}\${file}" -Access ReadOnly -StorageType ISO
-$volume = $disk | Get-Volume
-$drive = $volume.DriveLetter
-#& "${drive}:\VCExpress\setup.exe"
+Write-Verbose "Mounting disk image..."
+$disk = Mount-DiskImage -ImagePath $imagePath -Access ReadOnly -StorageType ISO
+try {
+	$volume = $disk | Get-Volume
+	$drive = $volume.DriveLetter
+	#& "${drive}:\VCExpress\setup.exe"
 
-#Copy-Item E:\VCExpress\*.* .
-#E:\VCExpress\WCU\vcredistmin_x86.exe  
-#Copy-Item E:\VCExpress\WCU\WinSDK\ . 
+	#Copy-Item E:\VCExpress\*.* .
+	#E:\VCExpress\WCU\vcredistmin_x86.exe  
+	#Copy-Item E:\VCExpress\WCU\WinSDK\ . 
 
-Write-Output "Copying files..."
-Remove-Item -LiteralPath temp -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -Name temp -Type Directory | Out-Null
-Copy-Item "${drive}:\VCExpress" temp -Recurse -Force
+	Write-Verbose "Copying files..."
+	Remove-Item -LiteralPath temp -Recurse -Force -ErrorAction SilentlyContinue
+	New-Item -Name temp -Type Directory | Out-Null
+	Copy-Item "${drive}:\VCExpress" temp -Recurse -Force
 
-attrib -r "temp\VCExpress\baseline.dat"
-# Disable optional components from installing
-Get-Content E:\VCExpress\baseline.dat | ForEach-Object {
-	if($_ -eq "DefaultSelected=1") {
-		"DefaultSelected=0"
-	} elseif($_ -like "*=silverlight*") {
-		$_; "DefaultSelected=0"
-	} else {
-		$_
-	}
-} | Out-File -Force "temp\VCExpress\baseline.dat"
+	attrib -r "temp\VCExpress\baseline.dat"
+	# Disable optional components from installing
+	Get-Content E:\VCExpress\baseline.dat | ForEach-Object {
+		if($_ -eq "DefaultSelected=1") {
+			"DefaultSelected=0"
+		} elseif($_ -like "*=silverlight*") {
+			$_; "DefaultSelected=0"
+		} else {
+			$_
+		}
+	} | Out-File -Force "temp\VCExpress\baseline.dat"
 
-Write-Output "Starting installer..."
-Start-Process -Wait -FilePath temp\VCExpress\setup.exe -ArgumentList /qb,/norestart,/log,$env:temp\vc.log -WorkingDirectory temp\VCExpress
-Remove-Item -LiteralPath temp -Recurse -Force -ErrorAction SilentlyContinue
+	Write-Verbose "Starting installer..."
+	Start-Process -Wait -FilePath temp\VCExpress\setup.exe -ArgumentList /qb,/norestart,/log,$env:temp\vc.log -WorkingDirectory temp\VCExpress
+	Remove-Item -LiteralPath temp -Recurse -Force -ErrorAction SilentlyContinue
+} finally {
+	Dismount-DiskImage -ImagePath $imagePath | Out-Null
+}
 
-Dismount-DiskImage -ImagePath "${scriptDir}\${file}" | Out-Null
-
-Write-Output "Success!"
+Write-Verbose "Success!"
