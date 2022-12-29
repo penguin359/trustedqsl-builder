832348ad5468102f0921f5c4b396e72873602c3e
Loren M. Lang <lorenl@north-winds.org>
2022-12-29 15:28:32 -0800

    Debug PATH environment

--- windows/setup-vs2013-appveyor.ps1
+++ windows/setup-vs2013-appveyor.ps1
@@ -13,10 +13,13 @@ choco install nasm trustedqsl
 
 dir $env:ProgramFiles
 dir ${env:ProgramFiles(x86)}
+dir ${env:ProgramFiles}\NASM
 
 Update-Path -Path (Join-Path $env:ProgramFiles "NASM") -Command nasm.exe
 Update-Path -Path (Join-Path ${env:ProgramFiles(x86)} "NASM") -Command nasm.exe
 
+echo "Old PATH: ${env:Path}"
 refreshenv
+echo "New PATH: ${env:Path}"
 
 Write-Verbose "Success!"
