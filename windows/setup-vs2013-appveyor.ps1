14adf8ef3bc9763efbbdeb07d3f2ee2d01474445
Loren M. Lang <lorenl@north-winds.org>
2022-12-29 14:19:29 -0800

    Attempting to debug the NASM install on AppVeyor

--- windows/setup-vs2013-appveyor.ps1
+++ windows/setup-vs2013-appveyor.ps1
@@ -4,11 +4,19 @@ $ErrorActionPreference = "Stop"
 
 Import-Module -Name (Join-Path $PSScriptRoot "common.psm1") -DisableNameChecking
 
+Install-Chocolatey
+
+refreshenv
+
 Write-Verbose "Installing other build tools..."
-choco --no-progress install nasm trustedqsl
+choco install nasm trustedqsl
+
+dir $env:ProgramFiles
+dir ${env:ProgramFiles(x86)}
 
 Update-Path -Path (Join-Path $env:ProgramFiles "NASM") -Command nasm.exe
+Update-Path -Path (Join-Path ${env:ProgramFiles(x86)} "NASM") -Command nasm.exe
 
-Update-LocalEnvironment
+refreshenv
 
 Write-Verbose "Success!"
