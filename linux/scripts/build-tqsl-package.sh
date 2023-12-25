d7b6a527f271738a6c7e53805ed4940588af2e76
Loren M. Lang <lorenl@north-winds.org>
2023-12-25 03:23:21 -0800

    Auto-detect latest version for downloading original package tarball

--- linux/scripts/build-tqsl-package.sh
+++ linux/scripts/build-tqsl-package.sh
@@ -47,27 +47,6 @@ else
 fi
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -qy
-#sudo DEBIAN_FRONTEND=noninteractive apt install -y wget dpkg-dev gcc cmake libssl-dev liblmdb-dev libdb5.3-dev libexpat1-dev zlib1g-dev libcurl4-gnutls-dev libwxgtk3.0-gtk3-dev libfuse2 trustedqsl #fuse3
-#rm -fr ~/raw
-#mkdir ~/raw
-#cd ~/raw
-#wget http://www.arrl.org/tqsl/tqsl-2.6.5.tar.gz
-#rm -fr tqsl-2.6.5
-#tar xvf tqsl-2.6.5.tar.gz
-#cd tqsl-2.6.5
-#cmake -B build -S .
-#cmake --build build
-#sudo cmake --install build
-#sudo ldconfig
-#tqsl --version 2>&1 | grep --color 'TQSL Version 2\..*'
-#sudo mkdir -p /usr/local/bin/
-## FIXME: Workaround needed on older Ubuntu releases
-## error: RPC failed; curl 56 GnuTLS recv error (-54): Error in the pull function.
-#git config --global http.postBuffer 1048576000
-#./linux-make-appimage.sh
-#./TQSL-x86_64.AppImage --version 2>&1 | grep --color 'TQSL Version 2\..*'
-##./linux-make-appimage.sh || true
-##./TQSL-x86_64.AppImage --version 2>&1 | grep --color 'TQSL Version 2\..*' || true
 cd
 
 
@@ -124,7 +103,8 @@ if [ "$branch" = "backport-trusty" ]; then
 else
 	sudo DEBIAN_FRONTEND=noninteractive apt install -qy ./trustedqsl-build-deps_*.deb
 fi
-wget http://archive.ubuntu.com/ubuntu/pool/universe/t/trustedqsl/trustedqsl_2.7.1.orig.tar.gz
+version="$(curl -qsSLf https://arrl.org/tqsl-download | sed -ne 's@.*/tqsl-\([0-9]\+\(\.[0-9]\+\)\+\)\.tar\.gz.*@\1@p')"
+wget "http://archive.ubuntu.com/ubuntu/pool/universe/t/trustedqsl/trustedqsl_${version}.orig.tar.gz"
 cd trustedqsl
 #lintian_opts="--fail-on error,warning"
 lintian_opts="--fail-on error"
